b7c6c581f7b557bb5d8425b8a53fd02e27709d23
Ryan Teoh <ryanteoh01@gmail.com>
2023-01-10 22:58:04 -0800

    Trigger the reconciler when function is invoked

--- python/faas-driver/func.py
+++ python/faas-driver/func.py
@@ -1,10 +1,22 @@
 from parliament import Context
 import digi
 
+reconciler_running = False
+
+def start_reconciler(): 
+    """
+    An idempotent function to start the digi reconciler.  
+    """
+    global reconciler_running 
+    if not reconciler_running:
+        digi.run()
+        reconciler_running = True
+
 def main(context: Context):
     if 'request' in context.keys():
-        digi.run()
-        return 'Done reconciliation', 200
+        start_reconciler()
+        return 'Reconciler running', 200
+        
     else:
         print("Empty request", flush=True)
         return "{}", 200
